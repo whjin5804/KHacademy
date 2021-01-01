e7bbef3c62a11ac89340c52c2d10bf488f94eb89
JINWONHO <whjin5804@naver.com>
2021-01-01 19:02:08 +0900

    최종문제

---
+++ Day09/src/Class_RealFinal/Reserve.java
@@ -0,0 +1,34 @@
+package Class_RealFinal;
+
+public class Reserve {
+
+	private String[] seatS = new String[10];
+	private String[] seatA = new String[10];
+	private String[] seatB = new String[10];
+	
+	public void reserve() {
+		System.out.print("좌석구분 S<1>, A<2>, B<3>>>");
+	}
+
+	public void inquiry() {
+		
+	}
+	
+	public void cancel() {
+		
+	}
+	
+	public void end() {
+		
+	}
+	
+	public void print() {
+		System.out.print("예약<1>, ");
+		System.out.print("조회<2>, ");
+		System.out.print("취소<3>, ");
+		System.out.print("끝내기<4>>> ");
+	}
+	
+	
+	
+}
